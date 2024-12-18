47549fb1340d846c6701bd9100a151937c9b4d41
Takayu <takayukoyama39h@gmail.com>
2024-12-18 21:09:53 +0900

    5章まで完了

--- app/members/page.tsx
+++ app/members/page.tsx
@@ -1,45 +1,10 @@
 import Image from "next/image";
+import { getMembersList } from "@/app/_libs/microcms";
+import { MEMBERS_LIST_LIMIT } from "@/app/_constants";
 import styles from "./page.module.css";
 
-const data = {
-    contents: [
-        {
-            id: "1",
-            image: {
-                url: "/img-member1.jpg",
-                width: 240,
-                height: 240,
-            },
-            name: "デイビッド・チャン",
-            position: "CEO",
-            profile: "グローバルテクノロジー企業での…あああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああ",
-        },
-        {
-            id: "2",
-            image: {
-                url: "/img-member2.jpg",
-                width: 240,
-                height: 240,
-            },
-            name: "エミリー・サンダース",
-            position: "COO",
-            profile: "グローバル企業での運営管理と…おおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおお",
-        },
-        {
-            id: "3",
-            image: {
-                url: "/img-member3.jpg",
-                width: 240,
-                height: 240,
-            },
-            name: "ジョン・ウィルソン",
-            position: "CTO",
-            profile: "先進技術の研究開発と…宇うううううううううううううううううううううううううううううううううううううううううううううううううううううううううううううううううううううううううううううううううううううううううううううううううううううううううううううううううううううううううううううううううううううううううううううううううううううううううううううううう「",
-        },
-    ],
-};
-
-export default function Page() {
+export default async function Page() {
+    const data = await getMembersList({ limit: MEMBERS_LIST_LIMIT });
     return (
         <div className={styles.container}>
             {data.contents.length === 0 ? (
--- app/page.tsx
+++ app/page.tsx
@@ -2,56 +2,26 @@ import styles from "./page.module.css";
 import Image from "next/image";
 import localImage from "../public/img-mv.jpg";
 import ButtonLink from "@/app/_components/ButtonLink";
-import { News } from "./_libs/microcms";
 import NewsList from "@/app/_components/NewsList";
+import { getNewsList } from "./_libs/microcms";
+import { TOP_NEWS_LIMIT } from "./_constants";
 
-const data: { contents: News[] } = {
-    contents: [
-        {
-            id: "1",
-            title: "渋谷にオフィスを移転しました",
-            category: {
-                name: "更新情報 ",
-            },
-            publishedAt: "2023/05/19",
-            createdAt: "2023/05/19",
-        },
-        {
-            id: "2",
-            title: "当社のCEOが業界リーダーTOP30に選出されました",
-            category: {
-                name: "更新情報 ",
-            },
-            publishedAt: "2023/05/19",
-            createdAt: "2023/05/19",
-        },
-        {
-            id: "3",
-            title: "テスト記事です",
-            category: {
-                name: "更新情報",
-            },
-            publishedAt: "2023/04/19",
-            createdAt: "2023/04/19",
-        },
-    ],
-};
-
-export default function Home() {
-    const sliceData = data.contents.slice(0, 2);
-    const name = "世界";
+export default async function Home() {
+    const data = await getNewsList({
+        limit: TOP_NEWS_LIMIT,
+    });
     return (
         <>
             <section className={styles.top}>
                 <div>
-                    <h1 className={styles.title}>テクノロジーの力で{name}を変える!</h1>
+                    <h1 className={styles.title}>テクノロジーの力で世界を変える</h1>
                     <p className={styles.description}>私たちは市場をリードしているグローバルテックカンパニーです。</p>
                 </div>
                 <Image className={styles.bgimg} src={localImage} alt="" />
             </section>
             <section className={styles.news}>
                 <h2 className={styles.newsTitle}>News</h2>
-                <NewsList news={sliceData} />
+                <NewsList news={data.contents} />
                 <div className={styles.newsLink}>
                     <ButtonLink href="/news">もっとみる</ButtonLink>
                 </div>
