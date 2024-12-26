236d7662cbb0e44976de05cee1c5d054f90020d0
Takayu <takayukoyama39h@gmail.com>
2024-12-26 13:56:12 +0900

    7章まで完了

--- app/news/page.tsx
+++ app/news/page.tsx
@@ -1,9 +1,20 @@
 import { getNewsList } from "@/app/_libs/microcms";
 import NewsList from "@/app/_components/NewsList";
+import Pagination from "@/app/_components/Pagination";
+import SearchField from "../_components/SearchField";
+import { NEWS_LIST_LIMIT } from "@/app/_constants";
 
 export default async function Page() {
     // 分割代入
-    const { contents: news } = await getNewsList();
+    const { contents: news, totalCount } = await getNewsList({
+        limit: NEWS_LIST_LIMIT,
+    });
 
-    return <NewsList news={news} />;
+    return (
+        <>
+            <SearchField />
+            <NewsList news={news} />
+            <Pagination totalCount={totalCount} />
+        </>
+    );
 }
