e6898b45ef8fa8978b5f293aa9df585c9726fbc8
Takayu <takayukoyama39h@gmail.com>
2024-12-27 23:25:33 +0900

    サイト全体にrevalidateを設定する

--- app/news/page.tsx
+++ app/news/page.tsx
@@ -4,8 +4,6 @@ import Pagination from "@/app/_components/Pagination";
 import SearchField from "../_components/SearchField";
 import { NEWS_LIST_LIMIT } from "@/app/_constants";
 
-export const revalidate = 60;
-
 export default async function Page() {
     // 分割代入
     const { contents: news, totalCount } = await getNewsList({
