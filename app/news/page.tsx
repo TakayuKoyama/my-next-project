ba4599b09d06f08e69556b06089d7290ced2619c
Takayu <takayukoyama39h@gmail.com>
2024-12-27 22:46:49 +0900

    ニュースにSSRを適用

--- app/news/page.tsx
+++ app/news/page.tsx
@@ -4,6 +4,8 @@ import Pagination from "@/app/_components/Pagination";
 import SearchField from "../_components/SearchField";
 import { NEWS_LIST_LIMIT } from "@/app/_constants";
 
+export const revalidate = 0;
+
 export default async function Page() {
     // 分割代入
     const { contents: news, totalCount } = await getNewsList({
